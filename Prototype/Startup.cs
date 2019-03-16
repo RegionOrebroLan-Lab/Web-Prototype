c315bbcb38b2bbbd4d2dcbf5acf9816a0ebc7c1d
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2019-03-16 15:52:31 +0100

    Add solution.

---
+++ Prototype/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Prototype
+{
+	public class Startup
+	{
+		#region Methods
+
+		public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment)
+		{
+			if(hostingEnvironment.IsDevelopment())
+				applicationBuilder.UseDeveloperExceptionPage();
+
+			applicationBuilder.UseDefaultFiles();
+			applicationBuilder.UseStaticFiles();
+		}
+
+		#endregion
+	}
+}
---
+++ Prototype/wwwroot/Scripts/Libraries/ReadMe.md
@@ -0,0 +1,3 @@
+https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.js
+https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js
+https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.js
